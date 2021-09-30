a57a1d3f71e7e03a16981f97f961a23b6b40a1e7
William Pepin <wpepin00@gmail.com>
2021-09-30 09:58:28 -0400

    commit de william avant la merde de alexandre

--- src/main.ino
+++ src/main.ino
@@ -12,23 +12,31 @@ Inclure les librairies de functions que vous voulez utiliser
 
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
 
-
-
 /* ****************************************************************************
 Variables globales et defines
 **************************************************************************** */
 // -> defines...
 // L'ensemble des fonctions y ont acces
+// Offset
+const float pi = 3.1415926535;
+const float WHEEL_CIRCONFERENCE = 2 * pi * 1.5;
+const float SPEED = .6;
+const bool REFERENCE_WHEEEL = 0;
+const bool SLAVE_WHEEL = 1;
+const float A_SPEED_RIGHT_WHEELOFFSET = 0;
+const float B_SPEED_RIGHT_WHEELOFFSET = 0.0267;
+const float B_SPEED_RIGHT_WHEELOFFSET_BASED_ON_SPEED = 0.0445 * SPEED;
+char RobotId;
 
-
+const float ROBOT_CIRCONFERENCE = 2 * pi * 3.6;
 
 /* ****************************************************************************
 Vos propres fonctions sont creees ici
 **************************************************************************** */
-void maFonction(){
+void maFonction()
+{
   // code
 }
-
 
 /* ****************************************************************************
 Fonctions d'initialisation (setup)
@@ -37,17 +45,99 @@ Fonctions d'initialisation (setup)
 // -> Se fait appeler seulement un fois
 // -> Generalement on y initilise les varibbles globales
 
-void setup(){
+void setup()
+{
   BoardInit();
+  RobotId = 'B';
+  delay(100);
 }
-
 
 /* ****************************************************************************
 Fonctions de boucle infini (loop())
 **************************************************************************** */
 // -> Se fait appeler perpetuellement suite au "setup"
 
-void loop() {
-  // SOFT_TIMER_Update(); // A decommenter pour utiliser des compteurs logiciels
-  delay(10);// Delais pour décharger le CPU
+void loop()
+{
+  avancer_distance(88);
+  tourner(90, 1);
+  avancer_distance(22);
+  tourner(90, 0);
+  avancer_distance(22);
+  tourner(90, 0);
+  avancer_distance(22);
+  tourner(90, 1);
+  exit(0);
+}
+
+// en pouce
+void avancer_distance(float distance)
+{
+  float tourDeRoue = distance / WHEEL_CIRCONFERENCE;
+  avancer();
+  float encoder = ENCODER_Read(0);
+  while ((encoder / 3200) < tourDeRoue)
+  {
+    encoder = ENCODER_Read(0);
+    Serial.println(encoder);
+  }
+  stop();
+  ENCODER_Reset(0);
+}
+
+void tourner(float angle, bool direction)
+{
+  ENCODER_Reset(0);
+  ENCODER_Reset(1);
+  float distance = ROBOT_CIRCONFERENCE * (angle / 360);
+  float tourDeRoue = distance / WHEEL_CIRCONFERENCE;
+  float encoder = 0;
+  if (direction)
+  {
+    Serial.println("droite");
+    MOTOR_SetSpeed(0, -SPEED / 2);
+    delay(10);
+    MOTOR_SetSpeed(1, SPEED / 2);
+    while ((encoder / 3200) < tourDeRoue)
+    {
+      encoder = ENCODER_Read(1);
+      Serial.println(encoder);
+    }
+  }
+  else
+  {
+    Serial.println("gauche");
+    MOTOR_SetSpeed(0, SPEED / 2);
+    delay(10);
+    MOTOR_SetSpeed(1, -(SPEED / 2));
+    while ((encoder / 3200) < tourDeRoue)
+    {
+      encoder = ENCODER_Read(0);
+      Serial.println(encoder);
+    }
+  }
+
+  stop();
+  ENCODER_Reset(0);
+}
+
+void avancer()
+{
+  if (RobotId == 'B')
+  {
+    MOTOR_SetSpeed(0, SPEED);
+    delay(10);
+    MOTOR_SetSpeed(1, SPEED + B_SPEED_RIGHT_WHEELOFFSET_BASED_ON_SPEED);
+  }
+}
+
+void stop()
+{
+  for (size_t i = 5; i > 0; i--)
+  {
+    float speed = i / 10;
+    MOTOR_SetSpeed(0, speed);
+    MOTOR_SetSpeed(1, speed + B_SPEED_RIGHT_WHEELOFFSET);
+    delay(100);
+  }
 }
