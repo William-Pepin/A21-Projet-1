d9e9c391b4ee73719f52699a85b04066d04b3107
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2021-10-25 16:32:29 -0400

    modifié :         src/main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,1 +1,53 @@
-test
+#include <LibRobus.h>
+#include <math.h>
+
+bool StateSignalSonore=false;
+bool StateQuilleTombee=false;
+bool StateCrossroad=false;
+int StateCouleur=0; //noir=0, blanc=1, rouge=2, etc.
+
+void setup()
+{
+  BoardInit();
+  delay(100);
+}
+
+void loop()
+{
+
+
+    if(StateSignalSonore=false){
+        //fonction suiveur
+        if(fonction sonore=True){
+            StateSignalSonore=True
+        }
+    }
+
+    if(StateSignalSonore=True){
+        if(StateQuilleTombee=False){
+            //Fonction balayage et infrarouge
+            //Fonction avancer et renverser quille
+            //Fonction reculer à la position de départ
+            //Fonction retour sur la ligne
+        }
+        if(StateQuilleTombee=True){
+            if(StateCrossroad=false){
+                //Fonction suiveur de ligne
+                //Fonction détection crossroad
+            }
+            if(StateCrossroad=true){
+                //Fonction aller au point couleur
+                StateCouleur=Mesurer la couleur
+                if(StateCouleur=0){
+                    //Apporter balle au point noir
+                }
+                else if(StateCouleur=1){
+                    //Apporter balle au point blanc
+                }
+                else if(StateCouleur=2){
+                    //Apporter balle au point rouge
+                }
+            }
+        }
+    }
+}
