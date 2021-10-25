43ec5e6f6944eeafe8af937668dda62f63764858
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2021-10-25 17:03:47 -0400

    modifié :         src/main.ino

--- src/main.ino
+++ src/main.ino
@@ -5,6 +5,10 @@ bool StateSignalSonore=false;
 bool StateQuilleTombee=false;
 bool StateCrossroad=false;
 int StateCouleur=0; //noir=0, blanc=1, rouge=2, etc.
+float AnglePIDGauche=20;
+float AnglePIDDroit=340;
+int StateDirection=1;  //0:ajustement à gauche, 1:Tout droit, 2:Ajustement à droite
+
 
 void setup()
 {
@@ -15,22 +19,23 @@ void setup()
 void loop()
 {
 
-
-    if(StateSignalSonore=false){
-        //fonction suiveur
-        if(fonction sonore=True){
-            StateSignalSonore=True
-        }
+    if(fonction MesureSonore=true){
+        StateSignalSonore=true;
     }
 
-    if(StateSignalSonore=True){
-        if(StateQuilleTombee=False){
+    if(StateSignalSonore=false){
+        //fonction MesureSuiveur
+        //fonction AjustementDirection
+    }
+
+    if(StateSignalSonore=true){
+        if(StateQuilleTombee=false){
             //Fonction balayage et infrarouge
             //Fonction avancer et renverser quille
             //Fonction reculer à la position de départ
-            //Fonction retour sur la ligne
+            //Fonction rotation jusqu'à ce que StateDirection=1
         }
-        if(StateQuilleTombee=True){
+        if(StateQuilleTombee=true){
             if(StateCrossroad=false){
                 //Fonction suiveur de ligne
                 //Fonction détection crossroad
@@ -49,5 +54,32 @@ void loop()
                 }
             }
         }
+    }
+}
+
+void mesuresuiveur(){
+    if (ENCODER_ReadReset(0)>1000 && ENCODER_ReadReset(1)>1000){
+        //prendre mesure du suiveur=mesure
+        if(mesure=x){
+            StateDirection=1;
+        }
+        else if(mesure=y){
+            StateDirection=0;
+        }
+        else if(mesure=z){
+            StateDirection=2;
+        }
+    }
+}
+
+void AjustementDirection(){
+    if(StateDirection=0){
+        Fonction PID avancer avec angle de 20 degré vers la gauche
+    }
+    if(StateDirection=1){
+        Fonction PID avancer droit
+    }
+    if(StateDirection=2){
+    Fonction PID avancer gauche
     }
 }
