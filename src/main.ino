031fd5f683dc76edbf0053f5cca1d5706e747d43
Alexandre Belleau <63368164+klaxonb871@users.noreply.github.com>
2021-10-27 17:55:20 -0400

    Added new function

--- src/main.ino
+++ src/main.ino
@@ -1,354 +1,104 @@
 #include <LibRobus.h>
 #include <math.h>
+#include <Wire.h>
 
-const double WHEEL_CIRCONFERENCE = 2 * PI * 1.5;
-const double ROBOT_CIRCONFERENCE_RIGHT = 2 * PI * 3.75;
-const double ROBOT_CIRCONFERENCE_LEFT = 2 * PI * 3.85;
-const double MIN_SPEED = 0.07;
+
+#define GROVE_ADR  (0x29)
+#define GROVE_CMD  (0x80)
+#define GROVE_ID   (0x12)
+#define INTEGRATIONTIME (0xEB)
+#define GAIN (0x01)
 
 void setup()
 {
   BoardInit();
   delay(100);
+ 
 }
 
 void loop()
 {
-  MOVEMENTS_Forward(86, .7);
-  MOVEMENTS_Turn(0, 90, .4);
-  MOVEMENTS_Forward(22, .5);
-  MOVEMENTS_Turn(1, 91, .4);
-  MOVEMENTS_Forward(22, .5);
-  MOVEMENTS_Turn(1, 90, .4);
-  MOVEMENTS_Forward(21, .5);
-  MOVEMENTS_Turn(0, 90, .4);
-  MOVEMENTS_Forward(15, .5);
-  MOVEMENTS_Turn(1, 45, .4);
-  MOVEMENTS_Forward(21, .5);
-  MOVEMENTS_Turn(0, 90, .4);
-  MOVEMENTS_Forward(29, .5);
-  MOVEMENTS_Turn(1, 45, .4);
-  MOVEMENTS_Forward(16, .5);
-  MOVEMENTS_Turn(1, 20, .4);
-  MOVEMENTS_Forward(31, .7);
-  MOVEMENTS_Turn(1, 180, .4);
-  exit(0);
+
+  
+  
+
 }
 
-/** Function to accelerate exponentially to a certain distance
 
-@param distance, distance travelled in inches
+void ColorCapteurWrite8(uint8_t reg, uint32_t value)
+ {
+    Wire.beginTransmission(GROVE_ADR);
+    Wire.write(GROVE_CMD | reg);
+    Wire.write(value & 0xFF);
+    Wire.endTransmission();
+}
 
-@param speed, represents direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-*/
-void MOVEMENTS_Accelerate(double distance, double speed)
+uint8_t ColorCapteurRead8(uint8_t reg)
+ {
+    Wire.beginTransmission(GROVE_ADR);
+    Wire.write(GROVE_CMD | reg);
+    Wire.endTransmission();
+
+    Wire.requestFrom(GROVE_ADR,1);
+    return Wire.read();
+}
+
+uint16_t ColorCapteurRead16(uint8_t reg) 
 {
-  double acceleration;
+    uint16_t x = 0; 
+    uint16_t t = 0;
 
-  MOVEMENTS_ResetEncoder();
-  int32_t targetEncoder = MOVEMENTS_EncoderForDistance(distance);
-  int32_t currentEncoder = MOVEMENTS_ReadEncoder();
+    Wire.beginTransmission(GROVE_ADR);
+    Wire.write(GROVE_CMD | reg);
+    Wire.endTransmission();
 
-  while (currentEncoder < targetEncoder)
-  {
-    currentEncoder = MOVEMENTS_ReadEncoder();
-    acceleration = MOVEMENTS_CalculateAcceleration(speed, currentEncoder, targetEncoder);
-
-    Serial.print("accelerate : ");
-    Serial.println(acceleration);
-
-    MOVEMENTS_Forward(acceleration);
-  }
-  MOVEMENTS_ResetEncoder();
+    Wire.requestFrom(GROVE_ADR, 2);
+    t = Wire.read();
+    x = Wire.read();
+    x <<= 8;
+    x |= t;
+    return x;
 }
 
-/** Function to deccelerate exponentially to a certain distance
-
-@param distance, distance travelled in inches
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-*/
-void MOVEMENTS_Deccelerate(double distance, double speed)
+void ColorCapteurEnable(void) 
 {
-  double decceleration;
-
-  MOVEMENTS_ResetEncoder();
-  int32_t targetEncoder = MOVEMENTS_EncoderForDistance(distance);
-  int32_t currentEncoder = MOVEMENTS_ReadEncoder();
-
-  while (currentEncoder < targetEncoder)
-  {
-    currentEncoder = MOVEMENTS_ReadEncoder();
-    decceleration = MOVEMENTS_CalculateDecceleration(speed, currentEncoder, targetEncoder);
-
-    Serial.print("Deccelerate : ");
-    Serial.println(decceleration);
-
-    MOVEMENTS_Forward(decceleration);
-  }
-  MOVEMENTS_Stop();
-  MOVEMENTS_ResetEncoder();
+    ColorCapteurWrite8(0x00, 0x01);
+    delay(3);
+    ColorCapteurWrite8(0x00, 0x01 | 0x02);
 }
 
-/** Function to go accelerate until reached speed,
- * then deccelerate at the desired distance. 
-
-@param distance, distance travelled in inches
-
-@param speed, represents direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-*/
-void MOVEMENTS_Forward(double distance, double speed)
+void ColorCapteurDisable(void) 
 {
-  MOVEMENTS_Accelerate(distance * .25, speed);
-
-  int32_t targetEncoder = MOVEMENTS_EncoderForDistance(distance * .50);
-  int32_t currentEncoder = MOVEMENTS_ReadEncoder();
-
-  while (currentEncoder < targetEncoder)
-  {
-    currentEncoder = MOVEMENTS_ReadEncoder();
-    MOVEMENTS_Forward(speed);
-  }
-
-  MOVEMENTS_Deccelerate(distance * .25, speed);
+    uint8_t reg = 0;
+    reg = ColorCapteurRead8(0x00);
+    ColorCapteurWrite8(0x00, reg & ~(0x01 | 0x02));
 }
 
-/** Function to accelerate exponentially to a certain distance while turning
-
-@param direction, 0 left, 1 right
-
-@param distance, distance travelled in inches
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-*/
-void MOVEMENTS_AccelerateTurn(bool direction, double distance, double speed)
+void ColorCapteurBegin(void) 
 {
-  double acceleration;
+    Wire.begin();
 
-  MOVEMENTS_ResetEncoder();
-  int32_t targetEncoder = MOVEMENTS_EncoderForDistance(distance);
-  int32_t currentEncoder = MOVEMENTS_ReadAbsEncoder();
+    /* Set default integration time and gain */
+    setIntegrationTime(INTEGRATIONTIME);
+    setGain(GAIN);
 
-  while (currentEncoder < targetEncoder)
-  {
-    currentEncoder = MOVEMENTS_ReadAbsEncoder();
-    acceleration = MOVEMENTS_CalculateAcceleration(speed, currentEncoder, targetEncoder);
-
-    if (direction)
-      MOVEMENTS_TurnRight(acceleration);
-    else
-      MOVEMENTS_TurnLeft(acceleration);
-  }
-  MOVEMENTS_ResetEncoder();
+    /* Note: by default, the device is in power down mode on bootup */
+    ColorCapteurEnable();
 }
 
-/** Function to deccelerate exponentially to a certain distance while turning
-
-@param direction, 0 left, 1 right
-
-@param distance, distance travelled in inches
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-*/
-void MOVEMENTS_DeccelerateTurn(bool direction, double distance, double speed)
+void setIntegrationTime(uint8_t time)
 {
-  double decceleration;
-
-  MOVEMENTS_ResetEncoder();
-  int32_t targetEncoder = MOVEMENTS_EncoderForDistance(distance);
-  int32_t currentEncoder = MOVEMENTS_ReadAbsEncoder();
-
-  while (currentEncoder < targetEncoder)
-  {
-    currentEncoder = MOVEMENTS_ReadAbsEncoder();
-    decceleration = MOVEMENTS_CalculateDecceleration(speed, currentEncoder, targetEncoder);
-
-    if (direction)
-      MOVEMENTS_TurnRight(decceleration);
-    else
-      MOVEMENTS_TurnLeft(decceleration);
-  }
-  MOVEMENTS_Stop();
-  MOVEMENTS_ResetEncoder();
+  ColorCapteurWrite8(0x01, time);
 }
 
-/** Function to turn smoothly to a certain angle
-
-@param direction, 0 left, 1 right
-
-@param angle, angle in degrees
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-*/
-void MOVEMENTS_Turn(bool direction, double angle, double speed)
+void setGain(uint8_t gain)
 {
-
-  double distance = MOVEMENTS_DistanceForAngle(direction, angle);
-
-  MOVEMENTS_AccelerateTurn(direction, distance * .30, speed);
-
-  int32_t targetEncoder = MOVEMENTS_EncoderForDistance(distance * .40);
-  int32_t currentEncoder = MOVEMENTS_ReadAbsEncoder();
-
-  while (currentEncoder < targetEncoder)
-  {
-    currentEncoder = MOVEMENTS_ReadAbsEncoder();
-    if (direction)
-      MOVEMENTS_TurnRight(speed);
-    else
-      MOVEMENTS_TurnLeft(speed);
-  }
-  MOVEMENTS_DeccelerateTurn(direction, distance * .30, speed);
+  ColorCapteurWrite8(0x0F, gain);
 }
 
-/** Function to correct the right wheel based on the left wheel
 
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
 
-*/
-double MOVEMENTS_SyncRightWheel(double speed)
-{
-  const double factor = 1.05;
 
-  double leftEncoder = abs(ENCODER_Read(0));
-  double rightEncoder = abs(ENCODER_Read(1));
-  if (leftEncoder == rightEncoder)
-    return speed;
-  else if (leftEncoder > rightEncoder + 1)
-    return speed * factor;
-  else if (leftEncoder < rightEncoder + 1)
-    return (speed - (speed * (factor - 1)));
-}
 
-/** Function to get the number of encoder ticks needed to reach a certain distance
 
-@param distance, distance travelled in inches
 
-*/
-double MOVEMENTS_EncoderForDistance(double distance)
-{
-  return (distance / WHEEL_CIRCONFERENCE) * 3200;
-}
-
-/** Function to get the number of distance to travel to reach a certain angle
-
-@param direction, 0 left, 1 right
-
-@param angle, angle in degrees
-
-*/
-double MOVEMENTS_DistanceForAngle(bool direction, double angle)
-{
-
-  if (direction)
-    return ROBOT_CIRCONFERENCE_RIGHT * (angle / 360);
-  return ROBOT_CIRCONFERENCE_LEFT * (angle / 360);
-}
-
-/** Function to calculate the acceleration based on the current encoder tick and the target encoder tick
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-
-@param currentEncoder, encoder tick at the instant
-
-@param targetEncoder, encoder tick to reach
-
-*/
-double MOVEMENTS_CalculateAcceleration(double speed, int32_t currentEncoder, int32_t targetEncoder)
-{
-  double acceleration = speed * currentEncoder / targetEncoder;
-
-  if (acceleration < MIN_SPEED)
-    acceleration = MIN_SPEED;
-
-  return acceleration;
-}
-
-/** Function to calculate the decceleartion based on the current encoder tick and the target encoder tick
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-
-@param currentEncoder, encoder tick at the instant
-
-@param targetEncoder, encoder tick to reach
-
-*/
-double MOVEMENTS_CalculateDecceleration(double speed, int32_t currentEncoder, int32_t targetEncoder)
-{
-  double decceleration = speed - speed * currentEncoder / targetEncoder;
-
-  if (decceleration < MIN_SPEED)
-    decceleration = MIN_SPEED;
-
-  return decceleration;
-}
-
-/** Function to read the main encoder */
-int32_t MOVEMENTS_ReadEncoder()
-{
-  return ENCODER_Read(0);
-}
-
-/** Function to read the absolute value of the main encoder */
-int32_t MOVEMENTS_ReadAbsEncoder()
-{
-  return abs(ENCODER_Read(0));
-}
-
-/** Function to read the main encoder and the slave encoder */
-int32_t MOVEMENTS_ResetEncoder()
-{
-  ENCODER_ReadReset(0);
-  ENCODER_ReadReset(1);
-}
-
-/** Function to turn both wheel at the same time
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-
-*/
-void MOVEMENTS_Forward(double speed)
-{
-  MOTOR_SetSpeed(0, speed);
-  MOTOR_SetSpeed(1, MOVEMENTS_SyncRightWheel(speed));
-}
-
-/** Function to turn left using both wheel
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-
-*/
-void MOVEMENTS_TurnLeft(double speed)
-{
-  MOTOR_SetSpeed(0, -speed);
-  MOTOR_SetSpeed(1, MOVEMENTS_SyncRightWheel(speed));
-}
-
-/** Function to turn right using both wheel
-
-@param speed, represents the starting direction and amplitude of PWM
-floating value between [-1.0, 1.0]
-*/
-void MOVEMENTS_TurnRight(double speed)
-{
-  MOTOR_SetSpeed(0, speed);
-  MOTOR_SetSpeed(1, -MOVEMENTS_SyncRightWheel(speed));
-}
-
-/** Function to stop both wheel at the same time */
-void MOVEMENTS_Stop()
-{
-  MOTOR_SetSpeed(0, 0);
-  MOTOR_SetSpeed(1, 0);
-}
