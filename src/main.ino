260fbb9df447cf7ffcff7ebf692179359385dbc4
Alexandre Belleau <63368164+klaxonb871@users.noreply.github.com>
2021-10-27 23:14:28 -0400

    if else pour afficher les couleurs

--- src/main.ino
+++ src/main.ino
@@ -7,17 +7,30 @@
 
 void setup()
 {
-  
   BoardInit();
   delay(100);
   ColorCapteurBegin();
- 
 }
 
 void loop()
 {
   struct RGB Couleurs;
   uint8_t Lecture = LectureCouleur(&Couleurs);
-  Serial.print(Lecture);
-  delay(2000);
+  if(Lecture == RED)
+  {
+    Serial.print("RED\n");
+  }
+  if(Lecture == GREEN)
+  {
+    Serial.print("GREEN\n");
+  }
+  if(Lecture == BLUE)
+  {
+    Serial.print("BLUE\n");
+  }
+  if(Lecture == YELLOW)
+  {
+    Serial.print("YELLOW\n");
+  }
+  delay(1000);
 }
