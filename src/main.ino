844e4ae6f98edf477da92171bbd34d6f7b382218
William Pepin <wpepin00@gmail.com>
2021-11-22 11:48:11 -0500

    Commit de max de teams

--- src/main.ino
+++ src/main.ino
@@ -1,12 +1,13 @@
 #include <LibRobus.h>
 #include <math.h>
 #include "CapteurCouleur.h"
-#include <CapteurSonar.h>
 
 const int redLED = 39;
 const int yellowLED = 41;
 const int greenLED = 43;
 const int blueLED = 45;
+const int greenButton = 47;
+const int RedButton = 49;
 
 int StateSignalSonore = 0;
 int StateQuilleTombee = 0;
@@ -22,7 +23,28 @@ const double ROBOT_CIRCONFERENCE_RIGHT = 2 * PI * 3.75;
 const double ROBOT_CIRCONFERENCE_LEFT = 2 * PI * 3.85;
 const double MIN_SPEED = 0.07;
 uint8_t couleur = 0;
-;
+
+bool isInChamber = false;
+int chamberNumber = 0;
+int timerRFID = 0;
+uint16_t audioTrack = 0;
+
+struct Drugs
+{
+  short red;
+  short bleu;
+  short green;
+};
+
+struct Patient
+{
+  char first_name[50];
+  char last_name[50];
+  short room_number;
+  char rfid_code[50];
+  struct Drugs daily_drugs;
+  bool daily_drugs_confirmation;
+};
 
 void setup()
 {
@@ -39,24 +61,126 @@ void setup()
   digitalWrite(redLED, LOW);
   pinMode(greenLED, OUTPUT);
   digitalWrite(greenLED, LOW);
+  data_initialisation();
+  pinMode(greenButton, INPUT_PULLUP);
+  pinMode(RedButton, INPUT_PULLUP);
 }
 
 void loop()
 {
-  detectObstacle();
-  //while (!(LineFollowerLeftDone && LineFollowerRightDone))
-  //{
-  //  AjustementDirection();
-  //  MesureSuiveur();
-  //  MesureSonar();
-  //  TimerUpdate();
-  //  Serial.println(StateQuilleTombee);
-  //}
-  //CouleurSequence();
-  //StateQuilleTombee = 0;
-  //LineFollowerLeftDone = false;
-  //LineFollowerRightDone = false;
+  if (isInChamber == true;)
+  {
+    AUDIO_PlayBlocking(0);
+
+    //on met un délais pour savoir si le client appuis dessus sinon on alerte les infirmière
+    while (RFID != -1 || timerRFID == 15000)
+    {
+      delay(50);
+      /*
+      scan RFID Lire identifie Patient
+      */
+      timerRFID += 50;
+    }
+
+    if (timerRFID < 15000)
+    {
+      //Appeler infirmière (allumer LED)
+      timerRFID == 0;
+    }
+
+    //chamberNumber est la chambre dans laquelle le robot est
+    if (chamberNumber == 0)
+    {
+
+      if (chamberNumber == Patient.room_number)
+      {
+        AUDIO_PlayBlocking(1); // Bonjour Mr Tremblay
+      }
+      else
+      {
+        //allumé LED
+      }
+    }
+    else if (chamberNumber == 1)
+    {
+      if (chamberNumber == Patient.room_number)
+      {
+        AUDIO_PlayBlocking(2); // Bonjour Mr Gagnon
+      }
+      else
+      {
+        //allumé LED
+      }
+    }
+    else if (chamberNumber == 2)
+    {
+      if (chamberNumber == Patient.room_number)
+      {
+        AUDIO_PlayBlocking(3); // Bonjour Mr Roy
+      }
+      else
+      {
+        //allumé LED
+      }
+    }
+    else if (chamberNumber == 3)
+    {
+      if (chamberNumber == Patient.room_number)
+      {
+        AUDIO_PlayBlocking(4); // Bonjour Mr Côté
+      }
+      else
+      {
+        //allumé LED
+      }
+    }
+  }
+
+  AUDIO_PlayBlocking(5); //Avez vous besoin d'assitance médicale immédiate
+  /*
+      Vérifie sur quel bouton le patient à appuyer
+    */
+  AUDIO_PlayBlocking(6); //Avez-vous eu votre dose de médicament quotidienne
+  /*
+      Vérifie sur quel bouton le patient à appuyer
+    */
+  if (Patient.daily_drugs_confirmation == false)
+  {
+    AUDIO_PlayBlocking(7); //Voici vos pillules ?
+    /*
+        Distribution de pillule
+     */
+  }
+  else
+  {
+    //Allumé LED
+  }
+
+  AUDIO_PlayBlocking(8); //Aimeriez-vous voir une infirmère pour tout autre raison non-urgente
+                         /*
+      Vérifie sur quel bouton le patient à appuyer
+    */
+
+  AUDIO_PlayBlocking(8) //Merci de votre compréhension, bye bye
+
+      isInChamber == false;
+  chamberNumber += 1;
 }
+
+// while (!(LineFollowerLeftDone && LineFollowerRightDone))
+// {
+//   AjustementDirection();
+//   MesureSuiveur();
+//   MesureSonar();
+//   TimerUpdate();
+//   Serial.println(StateQuilleTombee);
+// }
+// CouleurSequence();
+// StateQuilleTombee = 0;
+// LineFollowerLeftDone = false;
+// LineFollowerRightDone = false;
+}
+
 void TesterValeur()
 {
   int mesureGauche = analogRead(A7);
@@ -751,18 +875,125 @@ uint8_t GetCouleur()
   return Lecture;
 }
 
-void detectObstacle()
+void data_initialisation()
 {
-  if (SONAR_IsObstacleDetected())
+  struct Patient Tremblay;
+  Tremblay.last_name[0] = 'T';
+  Tremblay.last_name[1] = 'r';
+  Tremblay.last_name[2] = 'e';
+  Tremblay.last_name[3] = 'm';
+  Tremblay.last_name[4] = 'b';
+  Tremblay.last_name[5] = 'l';
+  Tremblay.last_name[6] = 'a';
+  Tremblay.last_name[7] = 'y';
+  Tremblay.last_name[8] = '\0';
+
+  Tremblay.first_name[0] = 'L';
+  Tremblay.first_name[1] = 'i';
+  Tremblay.first_name[2] = 'a';
+  Tremblay.first_name[3] = 'm';
+  Tremblay.first_name[4] = '\0';
+
+  Tremblay.room_number = 1;
+
+  Tremblay.rfid_code[0] = '1';
+  Tremblay.rfid_code[1] = '\0';
+
+  Tremblay.daily_drugs.red = 1;
+  Tremblay.daily_drugs.bleu = 2;
+  Tremblay.daily_drugs.green = 3;
+
+  Tremblay.daily_drugs_confirmation = false;
+
+  struct Patient Gagnon;
+  Gagnon.last_name[0] = 'G';
+  Gagnon.last_name[1] = 'a';
+  Gagnon.last_name[2] = 'g';
+  Gagnon.last_name[3] = 'n';
+  Gagnon.last_name[4] = 'o';
+  Gagnon.last_name[5] = 'n';
+  Gagnon.last_name[6] = '\0';
+
+  Gagnon.first_name[0] = 'T';
+  Gagnon.first_name[1] = 'h';
+  Gagnon.first_name[2] = 'o';
+  Gagnon.first_name[2] = 'm';
+  Gagnon.first_name[3] = 'a';
+  Gagnon.first_name[4] = 's';
+  Gagnon.first_name[5] = '\0';
+
+  Gagnon.room_number = 2;
+
+  Gagnon.rfid_code[0] = '2';
+  Gagnon.rfid_code[1] = '\0';
+
+  Gagnon.daily_drugs.red = 2;
+  Gagnon.daily_drugs.bleu = 2;
+  Gagnon.daily_drugs.green = 2;
+
+  Gagnon.daily_drugs_confirmation = false;
+
+  struct Patient Roy;
+  Roy.last_name[0] = 'R';
+  Roy.last_name[1] = 'o';
+  Roy.last_name[2] = 'y';
+  Roy.last_name[3] = '\0';
+
+  Roy.first_name[0] = 'O';
+  Roy.first_name[1] = 'l';
+  Roy.first_name[2] = 'i';
+  Roy.first_name[2] = 'v';
+  Roy.first_name[3] = 'i';
+  Roy.first_name[4] = 'a';
+  Roy.first_name[5] = '\0';
+
+  Roy.room_number = 3;
+
+  Roy.rfid_code[0] = '3';
+  Roy.rfid_code[1] = '\0';
+
+  Roy.daily_drugs.red = 3;
+  Roy.daily_drugs.bleu = 2;
+  Roy.daily_drugs.green = 1;
+
+  Roy.daily_drugs_confirmation = false;
+
+  struct Patient Cote;
+  Cote.last_name[0] = 'C';
+  Cote.last_name[1] = 'o';
+  Cote.last_name[2] = 't';
+  Cote.last_name[3] = 't';
+  Cote.last_name[4] = '\0';
+
+  Cote.first_name[0] = 'A';
+  Cote.first_name[1] = 'l';
+  Cote.first_name[2] = 'i';
+  Cote.first_name[2] = 'c';
+  Cote.first_name[3] = 'e';
+  Cote.first_name[4] = '\0';
+
+  Cote.room_number = 4;
+
+  Cote.rfid_code[0] = '4';
+  Cote.rfid_code[1] = '\0';
+
+  Cote.daily_drugs.red = 3;
+  Cote.daily_drugs.bleu = 3;
+  Cote.daily_drugs.green = 4;
+
+  Cote.daily_drugs_confirmation = false;
+}
+
+int gestionBouton()
+{
+  if (digitalRead(greenButton) == LOW)
   {
-    MOTOR_SetSpeed(0, 0);
-    MOTOR_SetSpeed(1, 0);
-    digitalWrite(greenLED, HIGH);
+    return 1;
   }
-  else
+  else if (digitalRead(redButton) == LOW)
   {
-    MOTOR_SetSpeed(0, 0.2);
-    MOTOR_SetSpeed(1, 0.2);
-    digitalWrite(greenLED, LOW);
+    return 0;
   }
+
+  return -1;
 }
